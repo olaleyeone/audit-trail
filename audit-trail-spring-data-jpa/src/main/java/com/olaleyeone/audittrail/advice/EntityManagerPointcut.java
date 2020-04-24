dc0358f2bacd7db86b2091fd8c9bd36b5805aefb
Olaleye Afolabi <olaleyeone@gmail.com>
2020-04-24 22:29:43 +0100

    added code comments

--- audit-trail-spring-data-jpa/src/main/java/com/olaleyeone/audittrail/advice/EntityManagerPointcut.java
+++ audit-trail-spring-data-jpa/src/main/java/com/olaleyeone/audittrail/advice/EntityManagerPointcut.java
@@ -6,13 +6,16 @@ interface EntityManagerPointcut {
 
     @Pointcut("execution(* javax.persistence.EntityManager.persist(Object))")
     default void persist() {
+        // Not to be implemented
     }
 
     @Pointcut("execution(* javax.persistence.EntityManager.merge(Object))")
     default void merge() {
+        // Not to be implemented
     }
 
     @Pointcut("execution(* javax.persistence.EntityManager.remove(Object))")
     default void remove() {
+        // Not to be implemented
     }
 }
